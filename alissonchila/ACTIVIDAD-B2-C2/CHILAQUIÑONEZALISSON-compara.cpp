0f15f2717d955aaf2e114d291e98cce52b0e2eb7
Samuel <JeanCor19>
2022-05-17 00:27:37 -0500

    Desde mi dispositivo

--- alissonchila/ACTIVIDAD-B2-C2/CHILAQUIÑONEZALISSON-compara.cpp
+++ alissonchila/ACTIVIDAD-B2-C2/CHILAQUIÑONEZALISSON-compara.cpp
@@ -1,23 +1,38 @@
+//==================================================
+//==>Nombre del programa: compara
+//==>Autor: CHILA QUIÑONEZ ALISSON ELENA
+//==> Archivo: CHILAQUIÑONEZALISSON-compara.cpp
+//==>Fecha de elaboración: 2022-04-01
+//==>Fecha ultima actualización: 2022-05-12
+//==================================================
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
- 	int n1, n2;
+ 	int QA_n1,QA_n2;
 	cout<<"\nEste programa compara dos numeros entre si."<<endl;
-	cout<<"\nIngrese el primer numero:"; cin>>n1;
-	cout<<"\nIngrese el segundo numero:"; cin>>n2;
-    if(n1>n2) {
-        cout<<"\n El numero mayo es "<<n1;
+	cout<<"\nIngrese el primer numero:"; cin>>QA_n1;
+	cout<<"\nIngrese el segundo numero:"; cin>>QA_n2;
+    if(QA_n1>QA_n2) {
+        cout<<"\n El numero mayo es "<<QA_n1;
     }   
    else{
-        if (n2>n1){
-            cout<<"\n El numero mayor es "<<n2;
+        if (QA_n2>QA_n1){
+            cout<<"\n El numero mayor es "<<QA_n2;
         }
-        else{
+        else
+//==================================================
+//==>Nombre del programa: compara
+//==>Autor: CHILA QUIÑONEZ ALISSON ELENA
+//==> Archivo: CHILAQUIÑONEZALISSON-compara.cpp
+//==>Fecha de elaboración: 2022-04-01
+//==>Fecha ultima actualización: 2022-05-12
+//==================================================
+
             cout<<"\nLos numeros son iguales.";
-        }
+        
     }
    
     
