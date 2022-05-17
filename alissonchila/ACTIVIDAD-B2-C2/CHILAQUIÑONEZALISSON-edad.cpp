e8020b66640d9510a378c0b78b739fff4f2ac7fc
Samuel <JeanCor19>
2022-05-17 01:49:37 -0500

    Dede mi movil

--- alissonchila/ACTIVIDAD-B2-C2/CHILAQUIÑONEZALISSON-edad.cpp
+++ alissonchila/ACTIVIDAD-B2-C2/CHILAQUIÑONEZALISSON-edad.cpp
@@ -1,28 +1,46 @@
+//==================================================
+//==>Nombre del programa: edad
+//==>Autor: CHILA QUIÑONEZ ALISSON ELENA
+//==> Archivo: CHILAQUIÑONEZALISSON-edad.cpp
+//==>Fecha de elaboración: 2022-04-01
+//==>Fecha ultima actualización: 2022-05-12
+//==================================================
 #include<iostream>
 using namespace std;
 
 int main ()
 {
-	int AA,MA,DA,AN,MN,DN,A,M,D;
+	int QE_AA,QE_MA,QE_DA,QE_AN,QE_MN,QE_DN,QE_A,QE_M,QE_D;
 	cout<<"Ingrese la fecha actual (AAAA MM DD): ";
-	cin>>AA>>MA>>DA;
+	cin>>QE_AA>>QE_MA>>QE_DA;
 	cout<<"Ingrese su fecha de nacimiento (AAAA MM DD): ";
-	cin>>AN>>MN>>DN;
-	if(DA>=DN){
-	D=DA-DN;
+	cin>>QE_AN>>QE_MN>>QE_DN;
+	if(QE_DA>=QE_DN){
+	QE_D=QE_DA-QE_DN;
 	}else{
-	DN=DN+30;
-	MA=MA-1;
-	D=DA-DN;
+	QE_DN=QE_DN+30;
+	QE_MA=QE_MA-1;
+	QE_D=QE_DA-QE_DN;
 	}
-	if(MA>=MN){
-	M=MA-MN;
+	if(QE_MA>=QE_MN){
+	QE_M=QE_MA-QE_MN;
 	}else{
-	MN=MN+12;
-	AA=AA-1;
-	M=MA-MN;
+	
+	QE_MN=QE_MN+12;
+	QE_AA=QE_AA-1;
+	QE_M=QE_MA-QE_MN;
 	}
-	A=AA-AN;
-	cout<<"Usted tiene "<<A<<" años "<<M<<" meses  y"<<D<<" dias ";
+	QE_A=QE_AA-QE_AN;
+//==================================================
+//==>Nombre del programa: edad
+//==>Autor: CHILA QUIÑONEZ ALISSON ELENA
+//==> Archivo: CHILAQUIÑONEZALISSON-edad.cpp
+//==>Fecha de elaboración: 2022-04-01
+//==>Fecha ultima actualización: 2022-05-12
+//==================================================
+
+	cout<<"Usted tiene "<<QE_A<<" años "<<QE_M<<" meses  y"<<QE_D<<" dias ";
+
+
 	return(0);
 }
