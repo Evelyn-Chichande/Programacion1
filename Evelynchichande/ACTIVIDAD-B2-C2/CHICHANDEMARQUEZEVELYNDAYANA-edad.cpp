abc863c745853c2efa36ff86a25adfb38195a961
EvelynChichande <evelynchichande1@gmail.com>
2022-05-17 18:38:46 -0500

    desde mi celular

--- Evelynchichande/ACTIVIDAD-B2-C2/CHICHANDEMARQUEZEVELYNDAYANA-edad.cpp
+++ Evelynchichande/ACTIVIDAD-B2-C2/CHICHANDEMARQUEZEVELYNDAYANA-edad.cpp
@@ -1,28 +1,46 @@
+//==>Nombre del programa: edad
+//==>Autor: CHICHANDE MARQUEZ EVELYN DAYANA
+//==> Archivo: CHICHANDEMARQUEZEVELYNDAYANA-edad.cpp
+//==>Fecha de elaboración: 2022-04-01
+//==>Fecha ultima actualización: 2022-05-12
+
+
 #include<iostream>
 using namespace std;
 
 int main ()
 {
-	int AA,MA,DA,AN,MN,DN,A,M,D;
+	int CHE_AA,CHE_MA,CHE_DA,CHE_AN,CHE_MN,CHE_DN,CHE_A,CHE_M,CHE_D;
 	cout<<"Ingrese la fecha actual (AAAA MM DD): ";
-	cin>>AA>>MA>>DA;
+	cin>>CHE_AA>>CHE_MA>>CHE_DA;
 	cout<<"Ingrese su fecha de nacimiento (AAAA MM DD): ";
-	cin>>AN>>MN>>DN;
-	if(DA>=DN){
-	D=DA-DN;
+	cin>>CHE_AN>>CHE_MN>>CHE_DN;
+	if(CHE_DA>=CHE_DN){
+	CHE_D=CHE_DA-CHE_DN;
 	}else{
-	DN=DN+30;
-	MA=MA-1;
-	D=DA-DN;
+	CHE_DN=CHE_DN+30;
+	CHE_MA=CHE_MA-1;
+	CHE_D=CHE_DA-CHE_DN;
 	}
-	if(MA>=MN){
-	M=MA-MN;
+	if(CHE_MA>=CHE_MN){
+	CHE_M=CHE_MA-CHE_MN;
 	}else{
-	MN=MN+12;
-	AA=AA-1;
-	M=MA-MN;
+	CHE_MN=CHE_MN+12;
+	CHE_AA=CHE_AA-1;
+	CHE_M=CHE_MA-CHE_MN;
 	}
-	A=AA-AN;
-	cout<<"Usted tiene "<<A<<" años "<<M<<" meses  y"<<D<<" dias ";
+	CHE_A=CHE_AA-CHE_AN;
+
+//==>Nombre del programa: edad
+//==>Autor: CHICHANDE MARQUEZ EVELYN DAYANA
+//==> Archivo: CHICHANDEMARQUEZEVELYNDAYANA-edad.cpp
+//==>Fecha de elaboración: 2022-04-01
+//==>Fecha ultima actualización: 2022-05-12
+
+
+
+
+
+	cout<<"Usted tiene "<<CHE_A<<" años "<<CHE_M<<" meses  y"<<CHE_D<<" dias ";
 	return(0);
 }
