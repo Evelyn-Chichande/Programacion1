bc5b11c67e13ad4f66b88b32660dfe383a3115ba
Samuel <JeanCor19>
2022-05-17 00:08:53 -0500

    desde mi cell

--- Evelynchichande/ACTIVIDAD-B2-C2/CHICHANDEMARQUEZEVELYNDAYANA-compara.cpp
+++ Evelynchichande/ACTIVIDAD-B2-C2/CHICHANDEMARQUEZEVELYNDAYANA-compara.cpp
@@ -1,18 +1,32 @@
+//==================================================
+//==>Nombre del programa: compara
+//==>Autor: CHICHANDE MARQUEZ EVELYN DAYANA
+//==> Archivo: CHICHANDEMARQUEZEVELYNDAYANA-compara.cpp
+//==>Fecha de elaboración: 2022-04-01
+//==>Fecha ultima actualización: 2022-05-12
+//==================================================
 #include<iostream>
 using namespace std;
 int main() {
-float a,b;
-cout<<"Ingrese a:" ; cin>>a;
-cout<<"Ingrese b:" ; cin>>b;
-if(a==b){
-cout<<"El valor de a="<<a<<":es igual a b="<<b<<endl;
+float CHE_a,CHE,b
+cout<<"Ingrese CHE_a:" ; cin>>CHE_a;
+cout<<"Ingrese CHE_b:" ; cin>>CHE_b;
+if(CHE_a==CHE_b){
+cout<<"El valor de CHE_a="<<CHE_a<<":es igual a CHE_b="<<CHE_b<<endl;
 	
 }else{
-if(a<b)	{ 
-cout<<"El valor de a="<<a<<":ES MENOR A b="<<b<<endl;
-}else{
-cout<<"El valor de b="<<b<<":ES MENOR A a="<<a<<endl;	
-} 
+if(CH_a<CH_b)	{ 
+cout<<"El valor de CHE_a="<<CH_a<<":ES MENOR A CHE_b="<<CH_b<<endl;
+}else
+//==================================================
+//==>Nombre del programa: compara
+//==>Autor: CHICHANDE MARQUEZ EVELYN DAYANA
+//==> Archivo: CHICHANDEMARQUEZEVELYNDAYANA-compara.cpp
+//==>Fecha de elaboración: 2022-04-01
+//==>Fecha ultima actualización: 2022-05-12
+//==================================================
+cout<<"El valor de CHE_b="<<CHE_b<<":ES MENOR A CHE_a="<<a<<endl;	
+ 
 }	
 return(0);	
 }
