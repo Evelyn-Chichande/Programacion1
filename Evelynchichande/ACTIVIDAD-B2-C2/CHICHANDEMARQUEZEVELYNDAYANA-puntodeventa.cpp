5517425e27d2493bc036ec86b09b484e314a9d4b
Samuel <JeanCor19>
2022-05-18 03:56:41 -0500

    desde mi cell

--- Evelynchichande/ACTIVIDAD-B2-C2/CHICHANDEMARQUEZEVELYNDAYANA-puntodeventa.cpp
+++ Evelynchichande/ACTIVIDAD-B2-C2/CHICHANDEMARQUEZEVELYNDAYANA-puntodeventa.cpp
@@ -1,29 +1,47 @@
+//==================================================
+//==>Nombre del programa: punto de venta
+//==>Autor: CHICHANDE MARQUEZ EVELYN DAYANA
+//==> Archivo: CHICHANDEMARQUEZEVELYNDAYANA-puntodeventa.cpp
+//==>Fecha de elaboración: 2022-04-30
+//==>Fecha ultima actualización: 2022-05-17
+//==================================================
+
+
 #include <iostream>
 using namespace std;
 int main ()
 {
-int c=0, P;
-float A=0, x, Tb, Piva, Pdsc, iva=0.12, desc=0.10, vt;
+int CHE_c=0, CHE_P;
+float CHE_A=0, CHE_x, CHE_Tb, CHE_Piva, CHE_Pdsc, CHE_iva=0.12, CHE_desc=0.10, CHE_vt;
 cout<<"Ingrese la cantidad de productos: ";
-cin>>P;
+cin>>CHE_P;
 do{
 cout<<"Ingrese el valor de los productos: ";
-cin>>x;
-c=c+1;
-A=A+x;
-Piva=A*iva;
-cout<<"El valor del iva de la compra es de: $"<<Piva<<endl;
-Pdsc=A*desc;
-cout<<"El valor del descuento de su compra es de: $"<<Pdsc<<endl;
-Tb=Piva+Pdsc;
-}while (c<P);
-if (Tb==A){
-cout<<"Su compra aplica para un descuento:"<<Tb<<endl;
-iva=Tb*Piva/100;
-desc=Tb*Pdsc/100;
+cin>>CHE_x;
+CHE_c=CHE_c+1;
+CHE_A=CHE_A+CHE_x;
+CHE_Piva=CHE_A*CHE_iva;
+cout<<"El valor del iva de la compra es de: $"<<CHE_Piva<<endl;
+CHE_Pdsc=CHE_A*CHE_desc;
+cout<<"El valor del descuento de su compra es de: $"<<CHE_Pdsc<<endl;
+CHE_Tb=CHE_Piva+CHE_Pdsc;
+}while (CHE_c<CHE_P);
+if (CHE_Tb==CHE_A){
+cout<<"Su compra aplica para un descuento:"<<CHE_Tb<<endl;
+CHE_iva=CHE_Tb*CHE_Piva/100;
+CHE_desc=CHE_Tb*CHE_Pdsc/100;
 }else{
-vt=A+Piva-Pdsc;
-cout<<"El valor total es de: $"<<vt<<endl;
+CHE_vt=CHE_A+CHE_Piva-CHE_Pdsc;
+
+
+        cout<<"//=====Nombre del programa: punto de venta  "<<endl;
+        cout<<"//=====Archivo: CHICHANDEMARQUEZEVELYNDAYANA-puntodeventa.cpp"<<endl;
+        cout<<"//=====Autor: CHICHANDE MARQUEZ EVELYN "<<endl;
+        cout<<"//=====Fecha de elaboración: 30-04-2022"<<endl;
+        cout<<"//=====Fecha de ultima actualización: 17-05-2022"<<endl;
+
+
+cout<<"El valor total es de: $"<<CHE_vt<<endl;
 }
 return(0);
 }
