85be66bd874706d6ff21a2a88bc7f8dd7b6a11d9
Samuel <JeanCor19>
2022-05-17 20:15:47 -0500

    desde mi telefono

--- alissonchila/ACTIVIDAD-B2-C2/CHILAQUIÑONEZALISSON-sumadevariosnumeros.cpp
+++ alissonchila/ACTIVIDAD-B2-C2/CHILAQUIÑONEZALISSON-sumadevariosnumeros.cpp
@@ -1,21 +1,37 @@
+//==================================================
+//==>Nombre del programa: sumadevariosnumeros
+//==>Autor: CHILA QUIÑONEZ ALISSON ELENA
+//==> Archivo: CHILAQUIÑONEZALISSON-sumadevariosnumeros.cpp
+//==>Fecha de elaboración: 2022-04-01
+//==>Fecha ultima actualización: 2022-05-12
+//==================================================
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    int c=0, n;
-    float s=0, x;
-    cout<<"Ingrese la cantidad de numeros a sumar:";
-    cin>>n;
+    int QE_c=0, QE_n;
+    float QE_s=0, QE_x;
+    cout<<"Ingrese la cantidad de números a sumar:";
+    cin>>QE_n;
     do{
   
-    cout<<"Ingrese un numero:";
-    cin>>x;
-    c=c+1;
-    s=s+x;
-       } while(c<n);
-            cout<<"Ingrese un numero para sumarlo: "<<s<<endl;
+    cout<<"Ingrese un número:";
+    cin>>QE_x;
+    QE_c=QE_c+1;
+    QE_s=QE_s+QE_x;
+       } while(QE_c<QE_n);
+//==================================================
+//==>Nombre del programa: sumadevariosnumeros
+//==>Autor: CHILA QUIÑONEZ ALISSON ELENA
+//==> Archivo: CHILAQUIÑONEZALISSON-sumadevariosnumeros.cpp
+//==>Fecha de elaboración: 2022-04-01
+//==>Fecha ultima actualización: 2022-05-12
+//==================================================
+
+            cout<<"Ingrese un numero para sumarlo: "<<QE_s<<endl;
         
         return(0);
 }
