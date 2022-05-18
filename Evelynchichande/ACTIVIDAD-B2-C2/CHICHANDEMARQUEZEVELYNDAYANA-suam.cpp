7203d9f60c89ff0226f508d88fd3a7c4e5e76717
Samuel <JeanCor19>
2022-05-18 03:20:55 -0500

    desde mi cell

--- Evelynchichande/ACTIVIDAD-B2-C2/CHICHANDEMARQUEZEVELYNDAYANA-suam.cpp
+++ Evelynchichande/ACTIVIDAD-B2-C2/CHICHANDEMARQUEZEVELYNDAYANA-suam.cpp
@@ -1,21 +1,35 @@
+//==================================================
+//==>Nombre del programa: suma de varios números
+//==>Autor: CORTEZ TORRES JEAN PIERRE
+//==> Archivo: CHICHANDEMARQUEZEVELYN-sumadevariosnúmeros.cpp
+//==>Fecha de elaboración: 2022-04-30
+//==>Fecha ultima actualización: 2022-05-17
+//==================================================
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    int c=0, n;
-    float s=0, x;
-    cout<<"Ingrese la cantidad de numeros a sumar:";
-    cin>>n;
+    int CHE_c=0, CHE_n;
+    float CHE_s=0, CHE_x;
+    cout<<"Ingrese la cantidad de números a sumar:";
+    cin>>CHE_n;
     do{
   
-    cout<<"Ingrese un numero:";
-    cin>>x;
-    c=c+1;
-    s=s+x;
-       } while(c<n);
-            cout<<"Ingrese un numero para sumarlo: "<<s<<endl;
+    cout<<"Ingrese un número:";
+    cin>>CHE_x;
+    CHE_c=CHE_c+1;
+ CHE_s=CHE_s+CHE_x;
+       } while(CHE_c<CHE_n);
+
+        cout<<"//=====Nombre del programa: suma de varios números "<<endl;
+        cout<<"//=====Archivo: CHICHANDEMARQUEZEVELYNDAYANA-edad.cpp"<<endl;
+        cout<<"//=====Autor: CHICHANDE MARQUEZ EVELYN "<<endl;
+        cout<<"//=====Fecha de elaboración: 30-04-2022"<<endl;
+        cout<<"//=====Fecha de ultima actualización: 17-05-2022"<<endl;
+
+            cout<<"Ingrese un número para sumarlo: "<< CHE_s<<endl;
         
         return(0);
 }
